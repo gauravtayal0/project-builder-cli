09e433eb8dba91fe9a24c311050e67f0162ea483
Gaurav Tayal <gaurav.tayal@cred.club>
2023-05-24 11:30:06 +0530

    remove source file

--- internal/utils/unzip.go
+++ internal/utils/unzip.go
@@ -2,12 +2,18 @@ package utils
 
 import (
 	"github.com/artdarek/go-unzip"
+	"os"
 )
 
 func UnZip(source, destination string) error {
-
 	uz := unzip.New(source, destination)
+
 	err := uz.Extract()
+	if err != nil {
+		return err
+	}
+
+	err = os.Remove(source)
 	if err != nil {
 		return err
 	}
